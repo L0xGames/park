f7cf385a5b84ecb2b373c4563111ff842fbbfabd
L0xGames <kiandousti@yahoo.de>
2019-09-18 02:07:24 +0200

    inspect

--- app/src/main/java/my/kian/myparkdisk/MainActivity.java
+++ app/src/main/java/my/kian/myparkdisk/MainActivity.java
@@ -11,7 +11,6 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
